655ec0e910766141dd185b8de4708e883c98e4f4
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-07-24 15:52:46 +0200

    build: initial build with Remix.js, TypeScript, styled-components

---
+++ app/shared/components/Header.styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  width: 100;
+  color: pink;
+`;
---
+++ app/shared/components/Header.tsx
@@ -0,0 +1,7 @@
+import { StyledHeader } from "./Header.styled";
+
+const Header = () => {
+  return <StyledHeader>this is a future header</StyledHeader>;
+};
+
+export default Header;
