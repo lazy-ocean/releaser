0e01dfc13eb57247d48592cea5b796d5eb5f041b
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-08-31 23:19:43 +0200

    feat: add meta data, og

--- app/root.tsx
+++ app/root.tsx
@@ -2,11 +2,19 @@ import type { MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 import styles from "~/shared/styles/global.css";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: MetaFunction = () => {
+  const description =
+    "With Spotify Releaser, you will never miss new music from your favourite artists!";
+  return {
+    viewport: "width=device-width,initial-scale=1",
+    charset: "utf-8",
+    description,
+    title: "Spotify Releaser",
+    "og:title": "Spotify Releaser",
+    "og:description": description,
+    "og:image": "meta_img.png",
+  };
+};
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
