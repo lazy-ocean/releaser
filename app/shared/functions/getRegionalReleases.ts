5b7f685640cbbea3a9a241d72eaf4fd22ac3d81f
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-09-15 18:06:25 +0200

    fix: deprecate location check for now cause it is slowing down everything

--- app/shared/functions/getRegionalReleases.ts
+++ app/shared/functions/getRegionalReleases.ts
@@ -1,19 +1,14 @@
 import type { ReleasesInterface } from "../types/types";
 import { getData, ENDPOINTS } from "../utils/getData";
 import { groupAlbumsByDate } from "./getRecentReleases";
-import getUserCountry from "./getUserCountry";
 
 const getRegionalReleases = async (
   accessToken: string
 ): Promise<{ albums: ReleasesInterface; location: string }> => {
-  const { country, countryCode } = await getUserCountry();
-  const data = await getData(
-    accessToken,
-    ENDPOINTS.RECENT_RELEASES(countryCode)
-  );
+  const data = await getData(accessToken, ENDPOINTS.RECENT_RELEASES("GB"));
   return {
     albums: groupAlbumsByDate(data.albums.items),
-    location: country,
+    location: "the UK",
   };
 };
 
